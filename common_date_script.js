00c37a42fdeefd718d898e35dde0de05055d99b7
daryn coleman <armysoldier3403@yahoo.com>
2025-07-24 09:17:51 -0500

    Delete common_date_script.js

--- common_date_script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const dateElement = document.getElementById('currentDate');
-    if (dateElement) { // Check if the element exists on the page
-        const today = new Date();
-        const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-        dateElement.textContent = today.toLocaleDateString('en-US', options);
-    }
-});
