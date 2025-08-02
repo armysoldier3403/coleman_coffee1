938c63bc0c9dacf747256599be84498e8bcaac1a
daryn coleman <armysoldier3403@yahoo.com>
2025-08-01 20:40:45 -0500

    Delete logout.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-// logout.php
-session_start(); // Start the session
-session_unset(); // Unset all session variables
-session_destroy(); // Destroy the session
-
-header("Location: index.html?login_status=logged_out"); // Redirect to home page with logout status
-exit();
-?>
