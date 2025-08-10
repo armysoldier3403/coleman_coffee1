801224ff8f3fac54b5995f8d4e866db9a4d0bb24
daryn coleman <armysoldier3403@yahoo.com>
2025-08-10 15:47:09 -0500

    Create date.php

---
+++ date.php
@@ -0,0 +1,6 @@
+<?php
+    // Step 1: Server-side PHP to get the current date and time
+    // This is for demonstration purposes and will not run in this environment.
+    date_default_timezone_set('America/Chicago'); // Example timezone for Fayette, IA
+    echo date('l, F j, Y \a\t g:i A');
+?>
